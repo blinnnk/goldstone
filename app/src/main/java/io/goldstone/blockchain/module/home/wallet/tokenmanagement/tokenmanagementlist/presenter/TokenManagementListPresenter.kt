ee06df23af381d41418c79ca18b619df922e2a87
Saith Kay <kaysaith@sina.com>
2018-04-18 14:20:57 +0800

    【优化】添加token界面的加载速度优化

--- app/src/main/java/io/goldstone/blockchain/module/home/wallet/tokenmanagement/tokenmanagementlist/presenter/TokenManagementListPresenter.kt
+++ app/src/main/java/io/goldstone/blockchain/module/home/wallet/tokenmanagement/tokenmanagementlist/presenter/TokenManagementListPresenter.kt
@@ -1,37 +1,63 @@
 package io.goldstone.blockchain.module.home.wallet.tokenmanagement.tokenmanagementlist.presenter
 
-import com.blinnnk.extension.forEachOrEnd
-import com.blinnnk.extension.isNull
-import com.blinnnk.extension.preventDuplicateClicks
-import com.blinnnk.extension.timeUpThen
+import com.blinnnk.extension.*
 import io.goldstone.blockchain.common.base.baserecyclerfragment.BaseRecyclerPresenter
 import io.goldstone.blockchain.kernel.commonmodel.MyTokenTable
 import io.goldstone.blockchain.module.common.walletgeneration.createwallet.model.WalletTable
 import io.goldstone.blockchain.module.home.home.view.MainActivity
 import io.goldstone.blockchain.module.home.wallet.tokenmanagement.tokenmanagementlist.model.DefaultTokenTable
+import io.goldstone.blockchain.module.home.wallet.tokenmanagement.tokenmanagementlist.view.TokenManagementListAdapter
 import io.goldstone.blockchain.module.home.wallet.tokenmanagement.tokenmanagementlist.view.TokenManagementListCell
 import io.goldstone.blockchain.module.home.wallet.tokenmanagement.tokenmanagementlist.view.TokenManagementListFragment
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.runOnUiThread
 
 /**
  * @date 25/03/2018 5:11 PM
  * @author KaySaith
  */
 
+// 获取数据后把默认列表防止到内存里加快每次访问的速度
+private var defaultTokenList: ArrayList<DefaultTokenTable>? = null
+
 class TokenManagementListPresenter(
   override val fragment: TokenManagementListFragment
 ) : BaseRecyclerPresenter<TokenManagementListFragment, DefaultTokenTable>() {
 
   override fun updateData() {
-    DefaultTokenTable.getTokens { defaultTokens ->
-      defaultTokens.forEachOrEnd { defaultToken, isEnd ->
-        val address = WalletTable.current.address
-        MyTokenTable.getTokensWith(address) { myTokens ->
-          defaultToken.isUsed = !myTokens.find { defaultToken.symbol == it.symbol }.isNull()
-          if (isEnd) fragment.asyncData = defaultTokens
+    defaultTokenList.isNull().isFalse {
+      fragment.asyncData = defaultTokenList
+      fragment.prepareMyDefaultTokens()
+    } otherwise {
+      fragment.prepareMyDefaultTokens()
+    }
+  }
+
+  private fun TokenManagementListFragment.prepareMyDefaultTokens() {
+    doAsync {
+      // 在异步线程更新数据
+      DefaultTokenTable.getTokens { defaultTokens ->
+        defaultTokens.forEachOrEnd { defaultToken, isEnd ->
+          val address = WalletTable.current.address
+          MyTokenTable.getTokensWith(address) { myTokens ->
+            defaultToken.isUsed = !myTokens.find { defaultToken.symbol == it.symbol }.isNull()
+            if (isEnd) {
+              // 在主线程更新 `UI`
+              context?.runOnUiThread {
+                asyncData.isNull().isTrue {
+                  asyncData = defaultTokens
+                } otherwise {
+                  diffAndUpdateSingleCellAdapterData<TokenManagementListAdapter>(defaultTokens)
+                }
+                defaultTokenList = defaultTokens
+              }
+            }
+          }
         }
       }
     }
   }
+
   companion object {
 
     private var needShowLoadingView = true
