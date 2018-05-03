6805890fba654ffe61636670d5c49015c536362c
Saith Kay <kaysaith@sina.com>
2018-05-03 12:29:06 +0800

    【优化】钱包设置界面返回后的键盘收起判断

--- app/src/main/java/io/goldstone/blockchain/module/home/wallet/walletsettings/walletsettingslist/presenter/WalletSettingsListPresenter.kt
+++ app/src/main/java/io/goldstone/blockchain/module/home/wallet/walletsettings/walletsettingslist/presenter/WalletSettingsListPresenter.kt
@@ -3,6 +3,7 @@ package io.goldstone.blockchain.module.home.wallet.walletsettings.walletsettings
 import android.support.v4.app.Fragment
 import com.blinnnk.extension.isFalse
 import com.blinnnk.extension.jump
+import com.blinnnk.util.SoftKeyboard
 import com.blinnnk.util.getParentFragment
 import io.goldstone.blockchain.GoldStoneApp
 import io.goldstone.blockchain.common.base.baserecyclerfragment.BaseRecyclerPresenter
@@ -28,90 +29,97 @@ import io.goldstone.blockchain.module.home.wallet.walletsettings.walletsettingsl
  */
 
 class WalletSettingsListPresenter(
-  override val fragment: WalletSettingsListFragment
+	override val fragment: WalletSettingsListFragment
 ) : BaseRecyclerPresenter<WalletSettingsListFragment, WalletSettingsListModel>() {
 
-  override fun updateData() {
-    val balanceText = WalletTable.current.balance?.formatCurrency() + " (${GoldStoneApp.currencyCode})"
-    fragment.asyncData = arrayListOf(
-      WalletSettingsListModel(WalletSettingsText.checkQRCode),
-      WalletSettingsListModel(WalletSettingsText.balance, balanceText),
-      WalletSettingsListModel(WalletSettingsText.walletName, WalletTable.current.name),
-      WalletSettingsListModel(WalletSettingsText.hint, "······"),
-      WalletSettingsListModel(WalletSettingsText.passwordSettings),
-      WalletSettingsListModel(WalletSettingsText.exportPrivateKey),
-      WalletSettingsListModel(WalletSettingsText.exportKeystore),
-      WalletSettingsListModel(WalletSettingsText.delete)
-    )
-  }
+	override fun onFragmentCreateView() {
+		super.onFragmentCreateView()
+		// 如果键盘在显示那么销毁键盘
+		fragment.activity?.apply { SoftKeyboard.hide(this) }
+	}
 
-  fun showTargetFragment(title: String) {
-    fragment.getParentFragment<WalletSettingsFragment>()?.apply {
-      headerTitle = title
-      presenter.showTargetFragmentByTitle(title)
-    }
-  }
+	override fun updateData() {
+		val balanceText =
+			WalletTable.current.balance?.formatCurrency() + " (${GoldStoneApp.currencyCode})"
+		fragment.asyncData = arrayListOf(
+			WalletSettingsListModel(WalletSettingsText.checkQRCode),
+			WalletSettingsListModel(WalletSettingsText.balance, balanceText),
+			WalletSettingsListModel(WalletSettingsText.walletName, WalletTable.current.name),
+			WalletSettingsListModel(WalletSettingsText.hint, "······"),
+			WalletSettingsListModel(WalletSettingsText.passwordSettings),
+			WalletSettingsListModel(WalletSettingsText.exportPrivateKey),
+			WalletSettingsListModel(WalletSettingsText.exportKeystore),
+			WalletSettingsListModel(WalletSettingsText.delete)
+		)
+	}
 
-  /** 分别从数据库和 `Keystore` 文件内删除掉用户钱包的所有数据 */
-  fun deleteWallet() {
-    fragment.context?.showAlertView(
-      WalletSettingsText.deleteInfoTitle,
-      WalletSettingsText.deleteInfoSubtitle,
-      !WalletTable.current.isWatchOnly
-    ) {
-      deleteWalletData(it?.text.toString())
-    }
-  }
+	fun showTargetFragment(title: String) {
+		fragment.getParentFragment<WalletSettingsFragment>()?.apply {
+			headerTitle = title
+			presenter.showTargetFragmentByTitle(title)
+		}
+	}
 
-  private fun deleteWalletData(password: String) {
-    fragment.getMainActivity()?.showLoadingView()
-    // get current wallet address
-    WalletTable.current.apply {
-      if (isWatchOnly) {
-        deleteWatchOnlyWallet(address)
-      } else {
-        fragment.deleteRoutineWallet(address, password)
-      }
-    }
-  }
+	/** 分别从数据库和 `Keystore` 文件内删除掉用户钱包的所有数据 */
+	fun deleteWallet() {
+		fragment.context?.showAlertView(
+			WalletSettingsText.deleteInfoTitle,
+			WalletSettingsText.deleteInfoSubtitle,
+			!WalletTable.current.isWatchOnly
+		) {
+			deleteWalletData(it?.text.toString())
+		}
+	}
 
-  private fun Fragment.deleteRoutineWallet(address: String, password: String) {
-    // delete `keystore` file
-    context?.deleteAccount(address, password) {
-      it.isFalse {
-        fragment.context?.alert("Wrong Password")
-        getMainActivity()?.removeLoadingView()
-        return@deleteAccount
-      }
-      // delete all records of this `address` in `myTokenTable`
-      MyTokenTable.deleteByAddress(address) {
-        TransactionTable.deleteByAddress(address) {
-          TokenBalanceTable.deleteByAddress(address) {
-            // delete wallet record in `walletTable`
-            WalletTable.deleteCurrentWallet {
-              // 删除 `push` 监听包地址不再监听用户删除的钱包地址
-              XinGePushReceiver.registerWalletAddressForPush()
-              activity?.jump<SplashActivity>()
-            }
-          }
-        }
-      }
-    }
-  }
+	private fun deleteWalletData(password: String) {
+		fragment.getMainActivity()?.showLoadingView()
+		// get current wallet address
+		WalletTable.current.apply {
+			if (isWatchOnly) {
+				deleteWatchOnlyWallet(address)
+			} else {
+				fragment.deleteRoutineWallet(address, password)
+			}
+		}
+	}
 
-  private fun deleteWatchOnlyWallet(address: String) {
-    MyTokenTable.deleteByAddress(address) {
-      TransactionTable.deleteByAddress(address) {
-        TokenBalanceTable.deleteByAddress(address) {
-          WalletTable.deleteCurrentWallet {
-            // 删除 `push` 监听包地址不再监听用户删除的钱包地址
-            XinGePushReceiver.registerWalletAddressForPush()
-            fragment.getMainActivity()?.removeLoadingView()
-            fragment.activity?.jump<SplashActivity>()
-          }
-        }
-      }
-    }
-  }
+	private fun Fragment.deleteRoutineWallet(address: String, password: String) {
+		// delete `keystore` file
+		context?.deleteAccount(address, password) {
+			it.isFalse {
+				fragment.context?.alert("Wrong Password")
+				getMainActivity()?.removeLoadingView()
+				return@deleteAccount
+			}
+			// delete all records of this `address` in `myTokenTable`
+			MyTokenTable.deleteByAddress(address) {
+				TransactionTable.deleteByAddress(address) {
+					TokenBalanceTable.deleteByAddress(address) {
+						// delete wallet record in `walletTable`
+						WalletTable.deleteCurrentWallet {
+							// 删除 `push` 监听包地址不再监听用户删除的钱包地址
+							XinGePushReceiver.registerWalletAddressForPush()
+							activity?.jump<SplashActivity>()
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private fun deleteWatchOnlyWallet(address: String) {
+		MyTokenTable.deleteByAddress(address) {
+			TransactionTable.deleteByAddress(address) {
+				TokenBalanceTable.deleteByAddress(address) {
+					WalletTable.deleteCurrentWallet {
+						// 删除 `push` 监听包地址不再监听用户删除的钱包地址
+						XinGePushReceiver.registerWalletAddressForPush()
+						fragment.getMainActivity()?.removeLoadingView()
+						fragment.activity?.jump<SplashActivity>()
+					}
+				}
+			}
+		}
+	}
 
 }
