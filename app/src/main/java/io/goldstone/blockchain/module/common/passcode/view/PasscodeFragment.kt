73386ca98d0913c706613a45e0747762019ebc30
Saith Kay <kaysaith@sina.com>
2018-05-03 21:28:55 +0800

    【优化】pincode 与键盘的冲突

--- app/src/main/java/io/goldstone/blockchain/module/common/passcode/view/PasscodeFragment.kt
+++ app/src/main/java/io/goldstone/blockchain/module/common/passcode/view/PasscodeFragment.kt
@@ -1,13 +1,18 @@
 package io.goldstone.blockchain.module.common.passcode.view
 
 import android.annotation.SuppressLint
+import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.RelativeLayout
 import android.widget.TextView
 import com.blinnnk.extension.*
 import com.blinnnk.uikit.ScreenSize
 import com.blinnnk.uikit.uiPX
+import com.blinnnk.util.SoftKeyboard
 import io.goldstone.blockchain.common.base.basefragment.BaseFragment
 import io.goldstone.blockchain.common.component.GradientType
 import io.goldstone.blockchain.common.component.GradientView
@@ -26,79 +31,84 @@ import org.jetbrains.anko.textColor
 
 class PasscodeFragment : BaseFragment<PasscodePresenter>() {
 
-  lateinit var container: RelativeLayout
-  private val keyboard by lazy { NumberKeyboard(context!!) }
-  private val passcodeInput by lazy { PasscodeInput(context!!) }
-  private var failedAttention: TextView? = null
+	lateinit var container: RelativeLayout
+	private val keyboard by lazy { NumberKeyboard(context!!) }
+	private val passcodeInput by lazy { PasscodeInput(context!!) }
+	private var failedAttention: TextView? = null
 
-  override val presenter = PasscodePresenter(this)
+	override val presenter = PasscodePresenter(this)
 
-  override fun AnkoContext<Fragment>.initView() {
-    container = relativeLayout {
-      isClickable = true
-      lparams(matchParent, matchParent)
-      GradientView(context).apply {
-        setStyle(GradientType.Blue)
-        lparams(matchParent, matchParent)
-      }.into(this)
+	override fun AnkoContext<Fragment>.initView() {
+		container = relativeLayout {
+			isClickable = true
+			lparams(matchParent, matchParent)
+			GradientView(context).apply {
+				setStyle(GradientType.Blue)
+				lparams(matchParent, matchParent)
+			}.into(this)
 
-      passcodeInput.apply {
-        y += ScreenSize.Height * 0.18f
-      }.into(this)
+			passcodeInput.apply {
+				y += ScreenSize.Height * 0.18f
+			}.into(this)
 
-      keyboard.into(this)
-      keyboard.apply {
-        setCenterInHorizontal()
-        setAlignParentBottom()
-        y -= ScreenSize.Height * 0.12f
-        setKeyboardClickEventByFrozenStatus()
-      }
-    }
-  }
+			keyboard.into(this)
+			keyboard.apply {
+				setCenterInHorizontal()
+				setAlignParentBottom()
+				y -= ScreenSize.Height * 0.12f
+				setKeyboardClickEventByFrozenStatus()
+			}
+		}
+	}
 
-  fun resetHeaderStyle() {
-    keyboard.resetCode()
-    passcodeInput.swipe()
-  }
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		activity?.apply { SoftKeyboard.hide(this) }
+	}
 
-  @SuppressLint("SetTextI18n")
-  fun showFailedAttention(content: String) {
-    failedAttention.isNull() isFalse {
-      failedAttention?.text = content
-    } otherwise {
-      failedAttention = TextView(context).apply {
-        y += 30.uiPX()
-        layoutParams = RelativeLayout.LayoutParams(matchParent, 20.uiPX())
-        textSize = 4.uiPX().toFloat()
-        textColor = Spectrum.red
-        typeface = GoldStoneFont.medium(context)
-        text = content
-        gravity = Gravity.CENTER_HORIZONTAL
-      }
-      failedAttention?.into(container)
-    }
-  }
+	fun resetHeaderStyle() {
+		keyboard.resetCode()
+		passcodeInput.swipe()
+	}
 
-  fun recoveryAfterFrezon() {
-    failedAttention?.let { container.removeView(it) }
-    keyboard.setKeyboardClickEventByFrozenStatus()
-    failedAttention = null
-    resetHeaderStyle()
-  }
+	@SuppressLint("SetTextI18n")
+	fun showFailedAttention(content: String) {
+		failedAttention.isNull() isFalse {
+			failedAttention?.text = content
+		} otherwise {
+			failedAttention = TextView(context).apply {
+				y += 30.uiPX()
+				layoutParams = RelativeLayout.LayoutParams(matchParent, 20.uiPX())
+				textSize = 4.uiPX().toFloat()
+				textColor = Spectrum.red
+				typeface = GoldStoneFont.medium(context)
+				text = content
+				gravity = Gravity.CENTER_HORIZONTAL
+			}
+			failedAttention?.into(container)
+		}
+	}
 
-  private fun NumberKeyboard.setKeyboardClickEventByFrozenStatus() {
-    // 检查是否处于冻结状态
-    presenter.isFrozenStatus { isFrozen ->
-      checkCode = Runnable {
-        if (isFrozen) return@Runnable
-        presenter.unlockOrAlert(getEnteredCode()) {
-          getEnteredCode().isEmpty() isTrue {
-            passcodeInput.recoveryStyle()
-          } otherwise {
-            passcodeInput.setEnteredStyle(getEnteredCode().lastIndex)
-          }
-        }
-      }
-    }
-  }
+	fun recoveryAfterFrezon() {
+		failedAttention?.let { container.removeView(it) }
+		keyboard.setKeyboardClickEventByFrozenStatus()
+		failedAttention = null
+		resetHeaderStyle()
+	}
+
+	private fun NumberKeyboard.setKeyboardClickEventByFrozenStatus() {
+		// 检查是否处于冻结状态
+		presenter.isFrozenStatus { isFrozen ->
+			checkCode = Runnable {
+				if (isFrozen) return@Runnable
+				presenter.unlockOrAlert(getEnteredCode()) {
+					getEnteredCode().isEmpty() isTrue {
+						passcodeInput.recoveryStyle()
+					} otherwise {
+						passcodeInput.setEnteredStyle(getEnteredCode().lastIndex)
+					}
+				}
+			}
+		}
+	}
 }
