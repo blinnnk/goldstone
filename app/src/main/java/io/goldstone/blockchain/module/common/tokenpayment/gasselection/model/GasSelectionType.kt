d97a0fb92b38062f1e79313b01e7729e4b9a0557
Saith Kay <kaysaith@sina.com>
2018-06-08 00:41:12 +0800

    【BUG】自定义矿工gasprice 和fast 相同的时候导致重复选中

--- app/src/main/java/io/goldstone/blockchain/module/common/tokenpayment/gasselection/model/GasSelectionType.kt
+++ app/src/main/java/io/goldstone/blockchain/module/common/tokenpayment/gasselection/model/GasSelectionType.kt
@@ -9,32 +9,38 @@ import io.goldstone.blockchain.crypto.toGwei
  * @date 2018/5/16 11:37 PM
  * @author KaySaith
  */
-
 data class GasSelectionModel(
+	val id: Int = 0,
 	val count: String = "0.000 ETH",
 	val info: String = "≈ 0.00 Gwei (Gas Price) * 0.000 (Gas Limit)",
 	var type: String = "calculating",
-	var isSelected: Boolean = false
+	var currentType: String = ""
 ) {
-
+	
 	constructor(
+		id: Int,
 		gWei: Double,
 		gasLimit: Double,
 		currentType: String
 	) : this(
+		id,
 		(gWei * gasLimit).toEthValue(), // count 转换过的
 		"≈ ${gWei.toGWeiValue()} Gwei (Gas Price) * ${gasLimit.toGasValue()} (Gas Limit)",
-		when (gWei.toGwei()) {
-			MinerFeeType.Cheap.value -> MinerFeeType.Cheap.content
-			MinerFeeType.Fast.value -> MinerFeeType.Fast.content
-			MinerFeeType.Recommend.value -> MinerFeeType.Recommend.content
-			else -> MinerFeeType.Custom.content
-		}, gWei.toGwei() == when (currentType) {
-			MinerFeeType.Fast.content -> MinerFeeType.Fast.value
-			MinerFeeType.Cheap.content -> MinerFeeType.Cheap.value
-			MinerFeeType.Recommend.content -> MinerFeeType.Recommend.value
-			else -> MinerFeeType.Custom.value
-		}
+		calculateType(id, gWei),
+		currentType
 	)
-
+	
+	companion object {
+		fun calculateType(id: Int, gWei: Double): String {
+			return if (id == 3) MinerFeeType.Custom.content
+			else {
+				when (gWei.toGwei()) {
+					MinerFeeType.Cheap.value -> MinerFeeType.Cheap.content
+					MinerFeeType.Fast.value -> MinerFeeType.Fast.content
+					MinerFeeType.Recommend.value -> MinerFeeType.Recommend.content
+					else -> MinerFeeType.Custom.content
+				}
+			}
+		}
+	}
 }
