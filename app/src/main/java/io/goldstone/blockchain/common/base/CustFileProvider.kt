0875a2cc64b28e1456e63f3f5a71ec05a7a1adb6
yanglihai <969870523@qq.com>
2018-08-21 13:47:10 +0800

    【功能】 下载升级功能完成

---
+++ app/src/main/java/io/goldstone/blockchain/common/base/CustFileProvider.kt
@@ -0,0 +1,10 @@
+package io.goldstone.blockchain.common.base
+
+import android.support.v4.content.FileProvider
+
+/**
+ * @date: 2018/8/21.
+ * @author: yanglihai
+ * @description:
+ */
+class CustFileProvider : FileProvider() {}
