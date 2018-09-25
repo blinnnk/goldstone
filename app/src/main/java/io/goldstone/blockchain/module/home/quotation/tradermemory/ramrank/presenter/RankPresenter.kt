36364d6288125030c1158e6f801f258509876aca
yanglihai <969870523@qq.com>
2018-09-25 21:38:58 +0800

    【BUG】 修复持有大户数据有时候不显示的bug

--- app/src/main/java/io/goldstone/blockchain/module/home/quotation/tradermemory/ramrank/presenter/RankPresenter.kt
+++ app/src/main/java/io/goldstone/blockchain/module/home/quotation/tradermemory/ramrank/presenter/RankPresenter.kt
@@ -4,7 +4,7 @@ import io.goldstone.blockchain.common.base.basefragment.BasePresenter
 import io.goldstone.blockchain.common.utils.LogUtil
 import io.goldstone.blockchain.kernel.network.GoldStoneAPI
 import io.goldstone.blockchain.module.home.quotation.tradermemory.ramrank.view.RankFragment
-import io.goldstone.blockchain.module.home.quotation.tradermemory.ramtrend.model.EOSRAMRankModel
+import org.jetbrains.anko.runOnUiThread
 
 /**
  * @date: 2018/9/25.
@@ -18,10 +18,13 @@ class RankPresenter(override val fragment: RankFragment) : BasePresenter<RankFra
 	}
 	
 	private fun getRank() {
+		
 		GoldStoneAPI.getEOSRAMRank( {
 			LogUtil.error("geteosramrank", it)
 		}) {
-			fragment.ramRankView.setData(it)
+			GoldStoneAPI.context.runOnUiThread {
+				fragment.ramRankView.setData(it)
+			}
 		}
 	}
 }
