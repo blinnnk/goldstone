2cf6f7cea934c7f164a9f8e122f694b20645d3c6
yanglihai <969870523@qq.com>
2018-11-05 14:51:18 +0800

    【功能】 大单列表创建

---
+++ app/src/main/java/io/goldstone/blockchain/module/home/rammarket/module/ramquotation/bigorder/presenter/BigOrderPresenter.kt
@@ -0,0 +1,12 @@
+package io.goldstone.blockchain.module.home.rammarket.module.ramquotation.bigorder.presenter
+
+import io.goldstone.blockchain.common.base.baserecyclerfragment.BaseRecyclerPresenter
+import io.goldstone.blockchain.module.home.rammarket.module.ramquotation.bigorder.view.BigOrderFragment
+
+/**
+ * @date: 2018/11/5.
+ * @author: yanglihai
+ * @description:
+ */
+class BigOrderPresenter(override val fragment: BigOrderFragment)
+	: BaseRecyclerPresenter<BigOrderFragment, String>() {}
